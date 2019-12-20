bb29be14ac8a015f1b6d4d8190fb343169481f50
fbeccia <45896239+beccia@users.noreply.github.com>
2019-12-20 14:30:00 +0100

    fixed whitespace

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -6,16 +6,16 @@ import SectionSelectImage from '../SectionSelectImage';
 
 const Home = () => {
         return (
-            <div className="home">
-                <div className="homeheader">
+            <div className="article">
+                <h1 className="header">
                 <Header text="Home" />
-                </div>
-                <div className="homepage-mainbody">
+                </h1>
+                <div className="article-main-body">
                     <SectionSelectImage domain="reviews" title="reviews"/>
                     <SectionSelectImage domain="articles" title="articles"/>
                 {/* <Link className="link-mainpage" to="/reviews" >Reviews</Link>  <br/>
                 <Link className="link-mainpage" to="/articles" >Articles</Link> */}
-                <div className="insidetext">
+                <div className="article-main-text">
                     <p>Welcome. The basic idea behind this website is to share thoughts wth all the bros who like videogames out there. 
                         Throughout my many years of playing games I noticed that even though I can enjoy most high rated triple A titles, sometimes mainstream professional videogame 
                         media just don't get it and there are gems being missed as such, silly discussion etc. so it's time to join in. I'll add a comment functionality soon. 
