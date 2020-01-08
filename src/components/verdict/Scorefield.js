fcd48ccf7b20102c8c4b8241c811fdf3c164c237
fbeccia <45896239+beccia@users.noreply.github.com>
2020-01-08 16:00:35 +0100

    added fallout 76 review

--- src/components/verdict/Scorefield.js
+++ src/components/verdict/Scorefield.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ScoreField = (props) => {
     return (
     <div className="scorefield">
-        {props.score}
+        <div className="score">{props.score}</div>
     </div>
     )
   }
