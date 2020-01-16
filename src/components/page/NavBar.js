8a5f0b1531b481b40610f0f4bb63699d2dd9943f
fbeccia <45896239+beccia@users.noreply.github.com>
2020-01-16 15:05:07 +0100

    added navbar update

--- src/components/page/NavBar.js
+++ src/components/page/NavBar.js
@@ -5,8 +5,10 @@ const NavBar = () => {
         return (
             <div className="topbar">
                     <Link to="/" className="nav-item">Home</Link>
+                    <Link to="/sections/reviews" className="nav-item">Reviews</Link>
+                    <Link to="/sections/articles" className="nav-item">Articles</Link>
                     <Link to="/about_this" className="nav-item">About this</Link>
-                    <Link to="/about_me" className="nav-item">About Me</Link>
+                    <Link to="/about_me" className="nav-item">About me</Link>
             </div>
         );
     } 
